f1da5e27456db3cfaf6a21f95ee555eb98de070a
laxika <laxika91@gmail.com>
2015-06-10 17:00:33 +0200

    added client side views

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,21 +1,34 @@
-var phonecatApp = angular.module('swordssorcery', []);
+var swordssorceryApp = angular.module('swordssorcery', ['ui.router']);
 
-phonecatApp.controller('NewsController', function ($scope, $http, $sce) {
-    $http.get('/news/last').success(function(data, status, headers, config) {
-        for(i = 0; i < data.length; i++) {
-            data[i].message = $sce.trustAsHtml(data[i].message);
-            data[i].title = $sce.trustAsHtml(data[i].title);
+swordssorceryApp.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/")
+    $stateProvider.state('index', {
+        url: "/",
+        views: {
+            "main": {
+                templateUrl: "/sub/index.html",
+                controller: function($scope, $http, $sce) {
+                    $http.get('/news/last').success(function(data, status, headers, config) {
+                        for(i = 0; i < data.length; i++) {
+                             data[i].message = $sce.trustAsHtml(data[i].message);
+                             data[i].title = $sce.trustAsHtml(data[i].title);
+                        }
+
+                        $scope.newslist = data;
+                    });
+                }
+            },
+            "right": {
+                templateUrl: "/sub/login.html",
+                controller: function($scope){
+                    $scope.user = {};
+
+                    $scope.submit = function() {
+                        $http.post('/user/login', $scope.user).success(function(data, status, headers, config) {
+                        });
+                    };
+                },
+            }
         }
-
-        $scope.newslist = data;
     });
-});
-
-phonecatApp.controller('LoginController', function ($scope, $http) {
-    $scope.user = {};
-
-    $scope.submit = function() {
-        $http.post('/user/login', $scope.user).success(function(data, status, headers, config) {
-        });
-    };
 });
