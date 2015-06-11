5bad1280065a840744245750d4764bcc60a7af42
laxika <laxika91@gmail.com>
2015-06-11 10:40:09 +0200

    removeed duplications

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,26 +1,28 @@
 var swordssorceryApp = angular.module('swordssorcery', ['ui.router']);
 
 swordssorceryApp.config(function($stateProvider, $urlRouterProvider){
+    mainView = {
+        templateUrl: "/sub/index.html",
+        resolve: {
+            newslist:  function($http){
+                return $http({method: 'GET', url: '/news/last'});
+            },
+        },
+        controller: function($scope, $http, $sce, newslist) {
+            for(i = 0; i < newslist.data.length; i++) {
+                 newslist.data[i].message = $sce.trustAsHtml(newslist.data[i].message);
+                 newslist.data[i].title = $sce.trustAsHtml(newslist.data[i].title);
+            }
+        
+            $scope.newslist = newslist.data;
+        }
+    };
+
     $urlRouterProvider.otherwise('/')
     $stateProvider.state('index', {
         url: '/',
         views: {
-            'main': {
-                templateUrl: "/sub/index.html",
-                resolve: {
-                    newslist:  function($http){
-                        return $http({method: 'GET', url: '/news/last'});
-                    },
-                },
-                controller: function($scope, $http, $sce, newslist) {
-                    for(i = 0; i < newslist.data.length; i++) {
-                         newslist.data[i].message = $sce.trustAsHtml(newslist.data[i].message);
-                         newslist.data[i].title = $sce.trustAsHtml(newslist.data[i].title);
-                    }
-
-                    $scope.newslist = newslist.data;
-                }
-            },
+            'main': mainView,
             'right': {
                 templateUrl: "/sub/login.html",
                 controller: function($scope, $http, $state, $rootScope) {
@@ -43,19 +45,7 @@ swordssorceryApp.config(function($stateProvider, $urlRouterProvider){
     }).state('home', {
         url: '/home/',
         views: {
-            'main': {
-                templateUrl: "/sub/index.html",
-                controller: function($scope, $http, $sce) {
-                    $http.get('/news/last').success(function(data, status, headers, config) {
-                        for(i = 0; i < data.length; i++) {
-                             data[i].message = $sce.trustAsHtml(data[i].message);
-                             data[i].title = $sce.trustAsHtml(data[i].title);
-                        }
-
-                        $scope.newslist = data;
-                    });
-                }
-            },
+            'main': mainView,
             'right': {
                 templateUrl: "/sub/menu.html",
                 controller: function($scope){
