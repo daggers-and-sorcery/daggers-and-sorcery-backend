7f8376585bbad089ceeb6c8c44105bd8575d5249
laxika <laxika91@gmail.com>
2015-06-25 10:00:36 +0200

    replaced timestamp with date at news

--- src/main/java/com/swordssorcery/server/model/News.java
+++ src/main/java/com/swordssorcery/server/model/News.java
@@ -3,22 +3,24 @@ package com.swordssorcery.server.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Document
 public class News {
 
     @Id
     private int id;
-    private long timestamp;
-    private String title;
-    private String message;
-    private String icon;
+    private Date date = new Date();
+    private String title = "";
+    private String message = "";
+    private String icon = "";
 
     public int getId() {
         return id;
     }
 
-    public long getTimestamp() {
-        return timestamp;
+    public Date getDate() {
+        return date;
     }
 
     public String getTitle() {
