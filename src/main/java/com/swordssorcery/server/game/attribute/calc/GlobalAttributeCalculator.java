0298a67c26e2c716570f93c400a7ca9116adae19
laxika <laxika91@gmail.com>
2015-07-02 19:24:31 +0200

    added attribute next level points calulation

--- src/main/java/com/swordssorcery/server/game/attribute/calc/GlobalAttributeCalculator.java
+++ src/main/java/com/swordssorcery/server/game/attribute/calc/GlobalAttributeCalculator.java
@@ -1,23 +1,29 @@
 package com.swordssorcery.server.game.attribute.calc;
 
 import com.swordssorcery.server.game.attribute.Attribute;
+import com.swordssorcery.server.game.attribute.AttributeCalculator;
 import com.swordssorcery.server.game.attribute.data.AttributeData;
+import com.swordssorcery.server.game.attribute.type.GeneralAttribute;
 import com.swordssorcery.server.game.attribute.type.SkillAttribute;
 import com.swordssorcery.server.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GlobalAttributeCalculator {
+public class GlobalAttributeCalculator implements AttributeCalculator {
 
     @Autowired
     private SkillAttributeCalculator skillAttributeCalculator;
+    @Autowired
+    private GeneralAttributeCalculator generalAttributeCalculator;
     @Autowired
     private DefaultAttributeCalculator defaultAttributeCalculator;
 
     public AttributeData calculateAttributeValue(User user, Attribute attribute) {
         if (attribute instanceof SkillAttribute) {
             return skillAttributeCalculator.calculateAttributeValue(user, attribute);
+        } else if (attribute instanceof GeneralAttribute) {
+            return generalAttributeCalculator.calculateAttributeValue(user, attribute);
         }
 
         return defaultAttributeCalculator.calculateAttributeValue(user, attribute);
