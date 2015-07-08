784e770b940ae2735aeb182b2344803a2d6faed0
laxika <laxika91@gmail.com>
2015-07-08 10:02:07 +0200

    finished inventory

--- src/main/java/com/swordssorcery/server/model/Inventory.java
+++ src/main/java/com/swordssorcery/server/model/Inventory.java
@@ -1,16 +1,26 @@
 package com.swordssorcery.server.model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Inventory {
 
-    private ArrayList<InventoryItem> itemList = new ArrayList<>();
+    private HashMap<Integer, Item> itemMap = new HashMap<>();
 
     private void addItem(int itemId, int amount) {
-
+        if(itemMap.containsKey(itemId)) {
+            itemMap.get(itemId).increaseAmount(amount);
+        } else {
+            itemMap.put(itemId, new Item(itemId, amount));
+        }
     }
 
     private void removeItem(int itemId, int amount) {
+        Item item = itemMap.get(itemId);
 
+        if(item.getAmount() - amount <= 0) {
+            itemMap.remove(itemId);
+        } else {
+            item.decreaseAmount(amount);
+        }
     }
 }
---
+++ src/main/java/com/swordssorcery/server/model/Item.java
@@ -0,0 +1,32 @@
+package com.swordssorcery.server.model;
+
+public class Item {
+
+    private final int id;
+    private int amount;
+
+    public Item(int id, int amount) {
+        this.id = id;
+        this.setAmount(amount);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void increaseAmount(int amount) {
+        this.amount += amount;
+    }
+
+    public void decreaseAmount(int amount) {
+        this.amount -= amount;
+    }
+}
