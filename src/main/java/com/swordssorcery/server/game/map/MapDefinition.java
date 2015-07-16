67e39eff0cb486531c3f82669890d70fd51ac56e
laxika <laxika91@gmail.com>
2015-07-16 14:52:08 +0200

    friendly reminder

--- src/main/java/com/swordssorcery/server/game/map/MapDefinition.java
+++ src/main/java/com/swordssorcery/server/game/map/MapDefinition.java
@@ -4,6 +4,8 @@ public class MapDefinition {
 
     private final int height;
     private final int width;
+
+    //TODO: Doesn't even need layers! Just MapCell[][] and Cell.isWalkable() etc!!!
     private final MapLayerDefinition[] layers;
 
     public MapDefinition(int width, int height, MapLayerDefinition[] layers) {
