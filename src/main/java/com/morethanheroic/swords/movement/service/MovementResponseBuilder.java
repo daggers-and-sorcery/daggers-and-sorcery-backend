8397e140e260da9791b94988631c32862d3e62aa
laxika <laxika91@gmail.com>
2015-08-04 17:13:51 +0200

    added movement response builder

--- src/main/java/com/morethanheroic/swords/movement/service/MovementResponseBuilder.java
+++ src/main/java/com/morethanheroic/swords/movement/service/MovementResponseBuilder.java
@@ -2,10 +2,24 @@ package com.morethanheroic.swords.movement.service;
 
 import com.morethanheroic.swords.common.response.Response;
 import com.morethanheroic.swords.user.domain.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MovementResponseBuilder {
 
-    public Response build(UserEntity user) {
-        return null;
+    private final PositionResponseBuilder positionResponseBuilder;
+
+    @Autowired
+    public MovementResponseBuilder(PositionResponseBuilder positionResponseBuilder) {
+        this.positionResponseBuilder = positionResponseBuilder;
+    }
+
+    public Response build(UserEntity user, boolean success) {
+        Response response = positionResponseBuilder.build(user);
+
+        response.setData("success", success);
+
+        return response;
     }
 }
--- src/main/java/com/morethanheroic/swords/movement/view/controller/CharacterMovementController.java
+++ src/main/java/com/morethanheroic/swords/movement/view/controller/CharacterMovementController.java
@@ -3,6 +3,7 @@ package com.morethanheroic.swords.movement.view.controller;
 import com.morethanheroic.swords.common.response.Response;
 import com.morethanheroic.swords.map.service.MapManager;
 import com.morethanheroic.swords.movement.service.MovementManager;
+import com.morethanheroic.swords.movement.service.MovementResponseBuilder;
 import com.morethanheroic.swords.movement.service.PositionResponseBuilder;
 import com.morethanheroic.swords.movement.view.request.MovementRequest;
 import com.morethanheroic.swords.user.domain.UserEntity;
@@ -25,22 +26,16 @@ public class CharacterMovementController {
     @Autowired
     private PositionResponseBuilder positionResponseBuilder;
 
+    @Autowired
+    private MovementResponseBuilder movementResponseBuilder;
+
     @RequestMapping(value = "/character/move", method = RequestMethod.POST)
     public Response move(UserEntity user, @RequestBody MovementRequest direction) {
         boolean result = movementManager.move(user, direction.getDirection());
 
         //TODO: combat here!
 
-        Response response = new Response();
-
-        response.setData("success", result);
-        response.setData("x", user.getXPosition());
-        response.setData("y", user.getYPosition());
-        response.setData("map", user.getMap().getId());
-
-        user.setPosition(user.getXPosition(), user.getYPosition());
-
-        return response;
+        return movementResponseBuilder.build(user, result);
     }
 
     @RequestMapping(value = "/character/position", method = RequestMethod.GET)
