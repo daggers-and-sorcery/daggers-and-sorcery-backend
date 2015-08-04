97ec8dcec7b5a67a6b54e1909469858f9768b5eb
laxika <laxika91@gmail.com>
2015-08-04 17:04:30 +0200

    added frontend for spawning

--- src/main/webapp/static/js/view/main/map-view.js
+++ src/main/webapp/static/js/view/main/map-view.js
@@ -9,6 +9,7 @@ module.exports = {
     },
     controller: function ($scope, $http, position) {
         $scope.position = position;
+        $scope.objects = position.spawnList;
 
         $scope.move = function(direction) {
             var directionData = {
@@ -19,6 +20,8 @@ module.exports = {
                 if(data.data.success) {
                     $scope.$broadcast('position', data.data);
                 }
+
+                $scope.objects = data.data.spawnList;
                 //TODO: Broadcast the result to the map, show the monsters etc on the new tile
             });
         }
