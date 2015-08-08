4bf7e76353c4897f466cc5fe8474f9183e01d0a3
laxika <laxika91@gmail.com>
2015-08-08 10:17:42 +0200

    more refacts

--- src/main/java/com/morethanheroic/swords/combat/service/calc/CombatCalculator.java
+++ src/main/java/com/morethanheroic/swords/combat/service/calc/CombatCalculator.java
@@ -1,82 +1,51 @@
 package com.morethanheroic.swords.combat.service.calc;
 
-import com.morethanheroic.swords.attribute.domain.CombatAttribute;
-import com.morethanheroic.swords.attribute.service.calc.GlobalAttributeCalculator;
 import com.morethanheroic.swords.combat.domain.Combat;
 import com.morethanheroic.swords.combat.domain.CombatResult;
 import com.morethanheroic.swords.combat.domain.Winner;
 import com.morethanheroic.swords.combat.service.CombatMessageBuilder;
-import com.morethanheroic.swords.combat.service.calc.attack.MonsterAttackCalculator;
-import com.morethanheroic.swords.combat.service.calc.attack.PlayerAttackCalculator;
 import com.morethanheroic.swords.monster.service.domain.MonsterDefinition;
 import com.morethanheroic.swords.user.domain.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
-
 @Service
 public class CombatCalculator {
 
     private final CombatMessageBuilder combatMessageBuilder;
-    private final GlobalAttributeCalculator globalAttributeCalculator;
-    private final MonsterAttackCalculator monsterAttackCalculator;
-    private final PlayerAttackCalculator playerAttackCalculator;
-
-    private final Random random = new Random();
+    private final TurnCalculator turnCalculator;
 
     @Autowired
-    public CombatCalculator(CombatMessageBuilder combatMessageBuilder, GlobalAttributeCalculator globalAttributeCalculator, MonsterAttackCalculator monsterAttackCalculator, PlayerAttackCalculator playerAttackCalculator) {
+    public CombatCalculator(TurnCalculator turnCalculator, CombatMessageBuilder combatMessageBuilder) {
+        this.turnCalculator = turnCalculator;
         this.combatMessageBuilder = combatMessageBuilder;
-        this.globalAttributeCalculator = globalAttributeCalculator;
-        this.monsterAttackCalculator = monsterAttackCalculator;
-        this.playerAttackCalculator = playerAttackCalculator;
     }
 
     public CombatResult doFight(UserEntity userEntity, MonsterDefinition monsterDefinition) {
         CombatResult result = new CombatResult();
         Combat combat = new Combat(userEntity, monsterDefinition);
 
-        result.addMessage(combatMessageBuilder.buildFightInitialisationMessage(combat.getMonsterDefinition().getName()));
-
-        while (combat.getPlayerHealth() > 0 && combat.getMonsterHealth() > 0) {
-            takeTurn(result, combat);
-        }
+        startFight(result, combat);
+        calculateFight(result, combat);
+        endFight(result, combat);
 
         return result;
     }
 
-    private void takeTurn(CombatResult result, Combat combat) {
-        if (combat.getTurn() == 0) {
-            takeZeroTurn();
-        } else {
-            takeSimpleTurn(result, combat);
-        }
-
-        combat.increaseTurn();
+    private void startFight(CombatResult result, Combat combat) {
+        result.addMessage(combatMessageBuilder.buildFightInitialisationMessage(combat.getMonsterDefinition().getName()));
     }
 
-    private void takeSimpleTurn(CombatResult result, Combat combat) {
-        int monsterInitialisation = combat.getMonsterDefinition().getInitiation();
-        int playerInitiation = globalAttributeCalculator.calculateActualValue(combat.getUserEntity(), CombatAttribute.INITIATION);
-
-        //TODO: messages
-        if (monsterInitialisation + random.nextInt(combat.getMonsterDefinition().getLevel()) >= playerInitiation + random.nextInt(1 /*player's level with it's weapon should be here instead of 1!*/)) {
-            //Monster attack first
-            monsterAttackCalculator.calculateMonsterAttack(result, combat);
-            if (combat.getPlayerHealth() > 0) {
-                playerAttackCalculator.calculatePlayerAttack(result, combat);
-            }
-        } else {
-            //Player attack first
-            playerAttackCalculator.calculatePlayerAttack(result, combat);
-            if (combat.getMonsterHealth() > 0) {
-                monsterAttackCalculator.calculateMonsterAttack(result, combat);
-            }
+    private void calculateFight(CombatResult result, Combat combat) {
+        while (combat.getPlayerHealth() > 0 && combat.getMonsterHealth() > 0) {
+            turnCalculator.takeTurn(result, combat);
         }
     }
 
-    private void takeZeroTurn() {
-        //TODO zero turn initial stuff
+    private void endFight(CombatResult result, Combat combat) {
+        if (result.getWinner() == Winner.PLAYER) {
+            //Add drops
+            //Add xp
+        }
     }
 }
