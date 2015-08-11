71ff0c2ab038a5035429981d77cb0d848d690fd8
laxika <laxika91@gmail.com>
2015-08-11 21:57:20 +0200

    fixed equipment attribute modifiers

--- src/main/java/com/morethanheroic/swords/item/service/domain/AttributeModifierDefinition.java
+++ src/main/java/com/morethanheroic/swords/item/service/domain/AttributeModifierDefinition.java
@@ -2,6 +2,10 @@ package com.morethanheroic.swords.item.service.domain;
 
 import com.morethanheroic.swords.attribute.enums.Attribute;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public abstract class AttributeModifierDefinition {
 
     private int amount;
--- src/main/webapp/static/js/constant/attribute-bonus-map-constant.js
+++ src/main/webapp/static/js/constant/attribute-bonus-map-constant.js
@@ -3,5 +3,6 @@ module.exports = require('js/app.js').constant('ATTRIBUTE_BONUS_MAP', {
     'RACIAL': 'Racial bonus',
     'SKILL': 'Skill bonus',
     'LEVEL': 'Skill level',
-    'GENERAL_ATTRIBUTE': 'General attribute bonus'
+    'GENERAL_ATTRIBUTE': 'General attribute bonus',
+    'EQUIPMENT': 'Equipment bonus'
 });
