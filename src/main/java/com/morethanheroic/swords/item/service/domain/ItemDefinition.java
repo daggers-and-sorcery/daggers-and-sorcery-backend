631c889b3f309de05cc560c740ef782528f731c9
laxika <laxika91@gmail.com>
2015-08-11 16:13:56 +0200

    added basic modifiers for equipments

--- src/main/java/com/morethanheroic/swords/item/service/domain/ItemDefinition.java
+++ src/main/java/com/morethanheroic/swords/item/service/domain/ItemDefinition.java
@@ -1,10 +1,16 @@
 package com.morethanheroic.swords.item.service.domain;
 
+import com.morethanheroic.swords.attribute.domain.BasicAttribute;
+import com.morethanheroic.swords.attribute.domain.CombatAttribute;
+import com.morethanheroic.swords.attribute.domain.GeneralAttribute;
+import com.morethanheroic.swords.attribute.domain.SkillAttribute;
+import com.morethanheroic.swords.attribute.enums.Attribute;
 import com.morethanheroic.swords.item.domain.ItemType;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @XmlRootElement(name = "item")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -14,6 +20,22 @@ public class ItemDefinition {
     private String name;
     private ItemType type;
     private int weight;
+
+    @XmlElementWrapper(name = "basic-modifiers")
+    @XmlElement(name = "modifier")
+    private ArrayList<BasicAttribute> basicModifiers;
+
+    @XmlElementWrapper(name = "combat-modifiers")
+    @XmlElement(name = "modifier")
+    private ArrayList<CombatAttribute> combatModifiers;
+
+    @XmlElementWrapper(name = "general-modifiers")
+    @XmlElement(name = "modifier")
+    private ArrayList<GeneralAttribute> generalModifiers;
+
+    @XmlElementWrapper(name = "skill-modifiers")
+    @XmlElement(name = "modifier")
+    private ArrayList<SkillAttribute> skillModifiers;
 
     public int getId() {
         return id;
@@ -43,5 +65,32 @@ public class ItemDefinition {
             default:
                 return false;
         }
+    }
+
+    public List<BasicAttribute> getBasicModifiers() {
+        return Collections.unmodifiableList(basicModifiers);
+    }
+
+    public List<CombatAttribute> getCombatModifiers() {
+        return Collections.unmodifiableList(combatModifiers);
+    }
+
+    public List<GeneralAttribute> getGeneralModifiers() {
+        return Collections.unmodifiableList(generalModifiers);
+    }
+
+    public List<SkillAttribute> getSkillModifiers() {
+        return Collections.unmodifiableList(skillModifiers);
+    }
+
+    public List<Attribute> getAllModifiers() {
+        List<Attribute> list = new ArrayList<>();
+
+        list.addAll(basicModifiers);
+        list.addAll(combatModifiers);
+        list.addAll(generalModifiers);
+        list.addAll(skillModifiers);
+
+        return Collections.unmodifiableList(list);
     }
 }
