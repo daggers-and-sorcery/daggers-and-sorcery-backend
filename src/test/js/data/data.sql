8a73867760537f775fddf22c5090aeffecd13624
laxika <laxika91@gmail.com>
2015-08-25 21:23:27 +0200

    added base load task for mysql to protractor testing

---
+++ src/test/js/data/data.sql
@@ -0,0 +1,16 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE swords.equipment;
+TRUNCATE TABLE swords.inventory;
+TRUNCATE TABLE swords.map_entity;
+TRUNCATE TABLE swords.map_object;
+TRUNCATE TABLE swords.news;
+TRUNCATE TABLE swords.user;
+
+INSERT INTO news
+SET id = 1, date = '1977-01-16 21:21:40', title = 'This is some test title', message = 'Lorem ipsum dolor sit ameth....', icon = 'bubble';
+INSERT INTO news
+SET id = 2, date = '2005-03-16 17:21:31', title = 'Some more tests', message = 'ZZzz hello world...', icon = 'bubble';
+
+
+SET FOREIGN_KEY_CHECKS = 1;
--- src/test/js/loaddb.bat
+++ src/test/js/loaddb.bat
@@ -1,1 +1,1 @@
-#C:\Users\Laxika\Developement\mongodb\mongodb-win32-x86_64-2008plus-2.4.6\bin\mongoimport.exe data\news.json --db swordssorcery --collection news --jsonArray
+mysql -u root --password= swords < data/data.sql
