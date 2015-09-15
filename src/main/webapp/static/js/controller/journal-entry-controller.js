a65521bf8cf61ac315502c82cb705668808acd22
laxika <laxika91@gmail.com>
2015-09-15 09:48:26 +0200

    working on journal display

---
+++ src/main/webapp/static/js/controller/journal-entry-controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function ($scope, journalEntryInfo) {
+    //$scope.type = 'item';
+    //$scope.list = journalInfo.data.data.journal_info;
+
+    console.log(journalEntryInfo);
+};
---
+++ src/main/webapp/static/js/router/state/journal-entry-state.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    name: 'journal_entry',
+    url: '/journal/:type/:entryId',
+    templateUrl: "/partial/main/journal_entry.html",
+    resolve: {
+        journalEntryInfo: function($http, $stateParams) {
+            console.log($stateParams);
+            return $http({method: 'GET', url: '/journal/entry/'+$stateParams.type+'/'+$stateParams.entryId});
+        }
+    },
+    controller: require('js/controller/journal-entry-controller.js')
+};
