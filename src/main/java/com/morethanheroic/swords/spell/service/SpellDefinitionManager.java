4abe007684844cecf9ecebdf6225deb17e5d5a7a
laxika <laxika91@gmail.com>
2015-10-03 11:07:30 +0200

    loading spells is finished

--- src/main/java/com/morethanheroic/swords/spell/service/SpellDefinitionManager.java
+++ src/main/java/com/morethanheroic/swords/spell/service/SpellDefinitionManager.java
@@ -1,12 +1,17 @@
 package com.morethanheroic.swords.spell.service;
 
+import com.morethanheroic.swords.combat.domain.CombatEffect;
 import com.morethanheroic.swords.definition.service.XMLDefinitionLoader;
+import com.morethanheroic.swords.effect.service.EffectDefinitionBuilder;
+import com.morethanheroic.swords.item.service.domain.ItemEffect;
 import com.morethanheroic.swords.spell.service.domain.RawSpellDefinition;
 import com.morethanheroic.swords.spell.service.domain.SpellDefinition;
+import com.morethanheroic.swords.spell.service.domain.SpellEffect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,10 +21,12 @@ public class SpellDefinitionManager {
     private HashMap<Integer, SpellDefinition> spellDefinitionMap = new HashMap<>();
 
     private final XMLDefinitionLoader xmlDefinitionLoader;
+    private final EffectDefinitionBuilder effectDefinitionBuilder;
 
     @Autowired
-    public SpellDefinitionManager(XMLDefinitionLoader xmlDefinitionLoader) {
+    public SpellDefinitionManager(XMLDefinitionLoader xmlDefinitionLoader, EffectDefinitionBuilder effectDefinitionBuilder) {
         this.xmlDefinitionLoader = xmlDefinitionLoader;
+        this.effectDefinitionBuilder = effectDefinitionBuilder;
     }
 
     @PostConstruct
@@ -27,7 +34,15 @@ public class SpellDefinitionManager {
         List<RawSpellDefinition> rawSpellDefinitionList = xmlDefinitionLoader.loadDefinitions(RawSpellDefinition.class, "classpath:data/spell/definition/", "classpath:data/spell/schema.xsd");
 
         for (RawSpellDefinition rawSpellDefinition : rawSpellDefinitionList) {
-            spellDefinitionMap.put(rawSpellDefinition.getId(), new SpellDefinition(rawSpellDefinition));
+            ArrayList<CombatEffect> effects = new ArrayList<>();
+
+            if(rawSpellDefinition.getEffectList() != null) {
+                for (SpellEffect effect : rawSpellDefinition.getEffectList()) {
+                    effects.add(effectDefinitionBuilder.build(effect));
+                }
+            }
+
+            spellDefinitionMap.put(rawSpellDefinition.getId(), new SpellDefinition(rawSpellDefinition, effects));
         }
     }
 
--- src/main/java/com/morethanheroic/swords/spell/service/domain/SpellDefinition.java
+++ src/main/java/com/morethanheroic/swords/spell/service/domain/SpellDefinition.java
@@ -1,14 +1,53 @@
 package com.morethanheroic.swords.spell.service.domain;
 
+import com.morethanheroic.swords.combat.domain.CombatEffect;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SpellDefinition {
 
     private final int id;
+    private final String name;
+    private final SpellType type;
+    private final boolean combatSpell;
+    private List<CombatEffect> combatEffects;
+    private List<SkillAttributeRequirementDefinition> skillRequirements = Collections.unmodifiableList(new ArrayList<>());
 
-    public SpellDefinition(RawSpellDefinition rawSpellDefinition) {
+    public SpellDefinition(RawSpellDefinition rawSpellDefinition, List<CombatEffect> combatEffects) {
         this.id = rawSpellDefinition.getId();
+        this.name = rawSpellDefinition.getName();
+        this.type = rawSpellDefinition.getType();
+        this.combatSpell = rawSpellDefinition.isCombatSpell();
+        this.combatEffects = combatEffects;
+
+        if (rawSpellDefinition.getSkillRequirements() != null) {
+            skillRequirements = Collections.unmodifiableList(rawSpellDefinition.getSkillRequirements());
+        }
     }
 
     public int getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public SpellType getType() {
+        return type;
+    }
+
+    public boolean isCombatSpell() {
+        return combatSpell;
+    }
+
+    public List<CombatEffect> getCombatEffects() {
+        return combatEffects;
+    }
+
+    public List<SkillAttributeRequirementDefinition> getSkillRequirements() {
+        return skillRequirements;
     }
 }
