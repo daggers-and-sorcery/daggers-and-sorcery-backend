1e487d4f1a12213a5560a9e815882cbd4cd10c46
laxika <laxika91@gmail.com>
2015-10-03 12:53:22 +0200

    added combat settings spell support

--- src/main/java/com/morethanheroic/swords/combatsettings/service/SettingsListResponseBuilder.java
+++ src/main/java/com/morethanheroic/swords/combatsettings/service/SettingsListResponseBuilder.java
@@ -7,6 +7,7 @@ import com.morethanheroic.swords.common.response.Response;
 import com.morethanheroic.swords.common.response.ResponseFactory;
 import com.morethanheroic.swords.item.service.ItemDefinitionManager;
 import com.morethanheroic.swords.monster.service.MonsterDefinitionManager;
+import com.morethanheroic.swords.spell.service.SpellDefinitionManager;
 import com.morethanheroic.swords.user.domain.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,12 +22,14 @@ public class SettingsListResponseBuilder {
     private final ResponseFactory responseFactory;
     private final MonsterDefinitionManager monsterDefinitionManager;
     private final ItemDefinitionManager itemDefinitionManager;
+    private final SpellDefinitionManager spellDefinitionManager;
 
     @Autowired
-    public SettingsListResponseBuilder(ResponseFactory responseFactory, MonsterDefinitionManager monsterDefinitionManager, ItemDefinitionManager itemDefinitionManager) {
+    public SettingsListResponseBuilder(ResponseFactory responseFactory, MonsterDefinitionManager monsterDefinitionManager, ItemDefinitionManager itemDefinitionManager, SpellDefinitionManager spellDefinitionManager) {
         this.responseFactory = responseFactory;
         this.monsterDefinitionManager = monsterDefinitionManager;
         this.itemDefinitionManager = itemDefinitionManager;
+        this.spellDefinitionManager = spellDefinitionManager;
     }
 
     public Response build(UserEntity userEntity, List<CombatSettingsDatabaseEntity> combatSettingList) {
@@ -37,10 +40,10 @@ public class SettingsListResponseBuilder {
             HashMap<String, Object> settingsData = new HashMap<>();
 
             SettingType type = combatSettingsDatabaseEntity.getType();
-            if(type == SettingType.ITEM) {
+            if (type == SettingType.ITEM) {
                 settingsData.put("use", itemDefinitionManager.getItemDefinition(combatSettingsDatabaseEntity.getSettingsId()).getName());
             } else {
-                //TODO: spell
+                settingsData.put("use", spellDefinitionManager.getSpellDefinition(combatSettingsDatabaseEntity.getSettingsId()).getName());
             }
             settingsData.put("type", type.name().toLowerCase());
 
--- src/main/java/com/morethanheroic/swords/combatsettings/service/UsableSpellsResponseBuilder.java
+++ src/main/java/com/morethanheroic/swords/combatsettings/service/UsableSpellsResponseBuilder.java
@@ -2,27 +2,49 @@ package com.morethanheroic.swords.combatsettings.service;
 
 import com.morethanheroic.swords.common.response.Response;
 import com.morethanheroic.swords.common.response.ResponseFactory;
+import com.morethanheroic.swords.spell.repository.dao.SpellDatabaseEntity;
+import com.morethanheroic.swords.spell.repository.domain.SpellMapper;
+import com.morethanheroic.swords.spell.service.SpellDefinitionManager;
+import com.morethanheroic.swords.spell.service.domain.SpellDefinition;
 import com.morethanheroic.swords.user.domain.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class UsableSpellsResponseBuilder {
 
     private final ResponseFactory responseFactory;
+    private final SpellMapper spellMapper;
+    private final SpellDefinitionManager spellDefinitionManager;
 
     @Autowired
-    public UsableSpellsResponseBuilder(ResponseFactory responseFactory) {
+    public UsableSpellsResponseBuilder(ResponseFactory responseFactory, SpellMapper spellMapper, SpellDefinitionManager spellDefinitionManager) {
         this.responseFactory = responseFactory;
+        this.spellMapper = spellMapper;
+        this.spellDefinitionManager = spellDefinitionManager;
     }
 
     public Response build(UserEntity userEntity) {
         Response response =  responseFactory.newResponse(userEntity);
+        ArrayList<HashMap<String, Object>> spelllist = new ArrayList<>();
 
-        //TODO: fill this after we added spells
-        response.setData("spellList", new ArrayList<>());
+        List<SpellDatabaseEntity> spells = spellMapper.getAllSpellsForUser(userEntity.getId());
+
+        for(SpellDatabaseEntity spell : spells) {
+            SpellDefinition spellDefinition = spellDefinitionManager.getSpellDefinition(spell.getSpellId());
+            HashMap<String, Object> spellInfo = new HashMap<>();
+
+            spellInfo.put("id", spellDefinition.getId());
+            spellInfo.put("name", spellDefinition.getName());
+
+            spelllist.add(spellInfo);
+        }
+
+        response.setData("spellList", spelllist);
 
         return response;
     }
