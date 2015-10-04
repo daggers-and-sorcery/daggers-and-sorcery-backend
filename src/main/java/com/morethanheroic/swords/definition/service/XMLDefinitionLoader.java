abb85d31480078725aa978d2b58da3cbbb3e9962
laxika <laxika91@gmail.com>
2015-10-04 17:16:03 +0200

    fixed resource problems

--- src/main/java/com/morethanheroic/swords/definition/service/XMLDefinitionLoader.java
+++ src/main/java/com/morethanheroic/swords/definition/service/XMLDefinitionLoader.java
@@ -2,7 +2,7 @@ package com.morethanheroic.swords.definition.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.InputStreamSource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
 
@@ -21,23 +21,35 @@ import java.util.List;
 @Service
 public class XMLDefinitionLoader {
 
-    private static final SchemaFactory schemaFactory =  SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    private static final SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
     @Autowired
     private ApplicationContext applicationContext;
 
     public List loadDefinitions(Class clazz, String resourcePath, String schemaPath) throws JAXBException, IOException, SAXException {
-        return unmarshallTargetFiles(buildUnmarshaller(clazz, schemaPath), getTargetFiles(resourcePath));
+        return unmarshallTargetFiles(buildUnmarshaller(clazz, schemaPath), resourcePath/*getTargetFiles(resourcePath)*/);
     }
 
-    private ArrayList unmarshallTargetFiles(Unmarshaller unmarshaller, File[] files) throws JAXBException {
+    private ArrayList unmarshallTargetFiles(Unmarshaller unmarshaller, String resourcePath /*, File[] files*/) throws JAXBException, IOException {
         ArrayList list = new ArrayList<>();
 
-        for (File file : files) {
-            list.add(unmarshaller.unmarshal(file));
+        for (int i = 1; i < 100; i++) {
+            Resource resource = applicationContext.getResource(resourcePath + i + ".xml");
+
+            if(!resource.exists()) {
+                return list;
+            }
+
+            list.add(unmarshaller.unmarshal(applicationContext.getResource(resourcePath + i + ".xml").getInputStream()));
         }
 
-        return list;
+
+        throw new IllegalStateException("Should be here! There is more items to read than the actual maxvalue!");
+        /*for (File file : files) {
+            list.add(unmarshaller.unmarshal(file));
+        }*/
+
+        //return list;
     }
 
     private File[] getTargetFiles(String resourcePath) throws IOException {
