b32b911ec7ad6ca6c877acd87be41a2323012bb0
laxika <laxika91@gmail.com>
2015-10-05 20:41:56 +0200

    fixed spawning

--- src/main/java/com/morethanheroic/swords/map/service/SpawnScheduler.java
+++ src/main/java/com/morethanheroic/swords/map/service/SpawnScheduler.java
@@ -3,6 +3,7 @@ package com.morethanheroic.swords.map.service;
 import com.morethanheroic.swords.map.domain.MapEntity;
 import com.morethanheroic.swords.map.service.task.SpawnTask;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +12,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@Lazy(false)
 public class SpawnScheduler {
 
     @Autowired
