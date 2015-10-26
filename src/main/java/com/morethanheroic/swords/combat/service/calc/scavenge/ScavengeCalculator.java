0d2644e4304eff2b76e2d7cf60103a592f087fc9
Andras <neandrw@gmail.com>
2015-10-26 19:30:26 +0100

    scavenging

---
+++ src/main/java/com/morethanheroic/swords/combat/service/calc/scavenge/ScavengeCalculator.java
@@ -0,0 +1,7 @@
+package com.morethanheroic.swords.combat.service.calc.scavenge;
+
+/**
+ * Created by neand_000 on 2015.10.24..
+ */
+public class ScavengeCalculator {
+}
---
+++ src/main/java/com/morethanheroic/swords/monster/service/loader/domain/RawScavengeDefinition.java
@@ -0,0 +1,7 @@
+package com.morethanheroic.swords.monster.service.loader.domain;
+
+/**
+ * Created by neand_000 on 2015.10.24..
+ */
+public class RawScavengeDefinition {
+}
