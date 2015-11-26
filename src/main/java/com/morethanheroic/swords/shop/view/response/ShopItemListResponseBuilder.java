2bd33ca1c423c1807faf53d3ebee1632b810a80c
laxika <laxika91@gmail.com>
2015-11-26 21:47:12 +0100

    DS-42: Added basic client side setup.

--- src/main/java/com/morethanheroic/swords/shop/view/response/ShopItemListResponseBuilder.java
+++ src/main/java/com/morethanheroic/swords/shop/view/response/ShopItemListResponseBuilder.java
@@ -3,6 +3,7 @@ package com.morethanheroic.swords.shop.view.response;
 import com.morethanheroic.swords.item.service.response.ItemEntryResponseBuilder;
 import com.morethanheroic.swords.response.domain.Response;
 import com.morethanheroic.swords.response.service.ResponseFactory;
+import com.morethanheroic.swords.shop.domain.ShopDefinition;
 import com.morethanheroic.swords.shop.domain.ShopEntity;
 import com.morethanheroic.swords.shop.domain.ShopItem;
 import com.morethanheroic.swords.user.domain.UserEntity;
@@ -26,15 +27,25 @@ public class ShopItemListResponseBuilder {
     public Response build(UserEntity userEntity, ShopEntity shopEntity) {
         Response response = responseFactory.newResponse(userEntity);
 
-        response.setData("itemList", buildItemList(shopEntity));
+        response.setData("shopDefinition", buildShopDefinition(shopEntity.getShopDefinition()));
+        response.setData("itemList", buildItemList(shopEntity.getAllItemsInShop()));
 
         return response;
     }
 
-    private List<Map<String, Object>> buildItemList(ShopEntity shopEntity) {
+    private Map<String, Object> buildShopDefinition(ShopDefinition shopDefinition) {
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("id", shopDefinition.getId());
+        result.put("name", shopDefinition.getName());
+
+        return result;
+    }
+
+    private List<Map<String, Object>> buildItemList(List<ShopItem> itemsInShop) {
         List<Map<String, Object>> result = new ArrayList<>();
 
-        for (ShopItem shopItem : shopEntity.getAllItemsInShop()) {
+        for (ShopItem shopItem : itemsInShop) {
             Map<String, Object> item = new HashMap<>();
 
             item.put("itemDefinition", itemEntryResponseBuilder.buildItemEntry(shopItem.getItem()));
