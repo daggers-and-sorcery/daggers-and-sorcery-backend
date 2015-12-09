eb120b469463275bade956d4eec86e285c86cdae
laxika <laxika91@gmail.com>
2015-12-09 20:50:28 +0100

    DS-120: Finished the money module tests.

--- swords-money/src/test/java/com/morethanheroic/swords/money/service/MoneyFacadeTest.java
+++ swords-money/src/test/java/com/morethanheroic/swords/money/service/MoneyFacadeTest.java
@@ -4,8 +4,10 @@ import com.google.common.collect.Lists;
 import com.morethanheroic.swords.money.domain.ConversionDefinition;
 import com.morethanheroic.swords.money.domain.Money;
 import com.morethanheroic.swords.money.domain.MoneyCalculationQuery;
+import com.morethanheroic.swords.money.domain.MoneyCalculationResult;
 import com.morethanheroic.swords.money.domain.MoneyDefinition;
 import com.morethanheroic.swords.money.service.cache.MoneyDefinitionCache;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -29,8 +31,8 @@ public class MoneyFacadeTest {
 
     private static final int BRONZE_CONVERSION_RATE = 1;
     private static final int SILVER_CONVERSION_RATE = 100;
-    private static final int GOLD_CONVERSION_RATE = 1000;
-    private static final int PLATINUM_CONVERSION_RATE = 10000;
+    private static final int GOLD_CONVERSION_RATE = 10000;
+    private static final int PLATINUM_CONVERSION_RATE = 1000000;
 
     @Mock
     private MoneyDefinitionCache moneyDefinitionCache;
@@ -38,19 +40,65 @@ public class MoneyFacadeTest {
     @InjectMocks
     private MoneyFacade underTest;
 
+    @Before
+    public void before() {
+        when(moneyDefinitionCache.getDefinition(Money.MONEY)).thenReturn(buildMoneyDefinitionMock());
+    }
+
     @Test
     @SuppressWarnings("checkstyle:magicnumber")
     public void testGetMoneyAmountShouldReturnCorrectAmount() throws Exception {
         final MoneyCalculationQuery moneyCalculationQuery = buildMoneyCalculationQueryMock(3102, 12, 34, 2);
-        when(moneyDefinitionCache.getDefinition(Money.MONEY)).thenReturn(buildMoneyDefinitionMock());
 
         final int result = underTest.getMoneyAmount(Money.MONEY, moneyCalculationQuery);
 
-        //3102 + 12 * 100 + 34 * 1000 + 2 * 10000
-        assertThat(result, is(58302));
+        //3102 + 12 * 100 + 34 * 10000 + 2 * 1000000
+        assertThat(result, is(2344302));
     }
 
-    //TODO: Add increase and decrease tests!
+    @Test
+    public void testDivideMoneyAmountShouldDivideCorrectly() {
+        final MoneyCalculationResult result = underTest.divideMoneyAmount(Money.MONEY, 92881192);
+
+        assertThat(result.getCurrency(BRONZE_COIN_ID), is(92));
+        assertThat(result.getCurrency(SILVER_COIN_ID), is(11));
+        assertThat(result.getCurrency(GOLD_COIN_ID), is(88));
+        assertThat(result.getCurrency(PLATINUM_COIN_ID), is(92));
+    }
+
+    @Test
+    public void testDivideMoneyAmountShouldDivideCorrectlyWhenWeHaveMoreThanHundredPlatinum() {
+        final MoneyCalculationResult result = underTest.divideMoneyAmount(Money.MONEY, 292881192);
+
+        assertThat(result.getCurrency(BRONZE_COIN_ID), is(92));
+        assertThat(result.getCurrency(SILVER_COIN_ID), is(11));
+        assertThat(result.getCurrency(GOLD_COIN_ID), is(88));
+        assertThat(result.getCurrency(PLATINUM_COIN_ID), is(292));
+    }
+
+    @Test
+    public void testIncreaseMoneyAmountShouldReturnCorrectlyIncreasedResult() {
+        final MoneyCalculationQuery moneyCalculationQuery = buildMoneyCalculationQueryMock(3102, 12, 34, 2);
+
+        final MoneyCalculationResult result = underTest.increaseMoneyAmount(Money.MONEY, moneyCalculationQuery, 4421);
+
+        assertThat(result.getCurrency(BRONZE_COIN_ID), is(23));
+        assertThat(result.getCurrency(SILVER_COIN_ID), is(87));
+        assertThat(result.getCurrency(GOLD_COIN_ID), is(34));
+        assertThat(result.getCurrency(PLATINUM_COIN_ID), is(2));
+    }
+
+    @Test
+    public void testDecreaseMoneyAmountShouldReturnCorrectlyIncreasedResult() {
+        final MoneyCalculationQuery moneyCalculationQuery = buildMoneyCalculationQueryMock(3102, 12, 34, 2);
+
+        final MoneyCalculationResult result = underTest.decreaseMoneyAmount(Money.MONEY, moneyCalculationQuery, 87922);
+
+        assertThat(result.getCurrency(BRONZE_COIN_ID), is(80));
+        assertThat(result.getCurrency(SILVER_COIN_ID), is(63));
+        assertThat(result.getCurrency(GOLD_COIN_ID), is(25));
+        assertThat(result.getCurrency(PLATINUM_COIN_ID), is(2));
+    }
 
     private MoneyDefinition buildMoneyDefinitionMock() {
         return MoneyDefinition.builder()
