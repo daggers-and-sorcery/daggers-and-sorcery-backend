ec4f5dd0dbb3a503bc424b7b644fe1516b72169c
laxika <laxika91@gmail.com>
2015-12-11 21:59:26 +0100

    DS-92: added base for recipe facade.

--- swords-recipe/src/main/java/com/morethanheroic/swords/recipe/service/RecipeFacade.java
+++ swords-recipe/src/main/java/com/morethanheroic/swords/recipe/service/RecipeFacade.java
@@ -1,7 +1,9 @@
 package com.morethanheroic.swords.recipe.service;
 
 import com.morethanheroic.swords.recipe.domain.RecipeDefinition;
+import com.morethanheroic.swords.recipe.domain.RecipeEntity;
 import com.morethanheroic.swords.recipe.service.cache.RecipeDefinitionCache;
+import com.morethanheroic.swords.user.domain.UserEntity;
 import com.sun.istack.internal.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +19,21 @@ public class RecipeFacade {
     @NotNull
     private final RecipeDefinitionCache recipeDefinitionCache;
 
-    public RecipeDefinition getDefinition(int id) {
-        return recipeDefinitionCache.getDefinition(id);
+    public RecipeDefinition getDefinition(int recipeId) {
+        return recipeDefinitionCache.getDefinition(recipeId);
+    }
+
+    public RecipeEntity getEntity(UserEntity userEntity, int recipeId) {
+        //todo
+        return null;
+    }
+
+    public void learnRecipe(UserEntity userEntity, RecipeDefinition recipeDefinition) {
+        //todo
+    }
+
+    public boolean hasRecipeLearned(UserEntity userEntity, RecipeDefinition recipeDefinition) {
+        //todo
+        return false;
     }
 }
