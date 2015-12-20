38b15e66a681b0dee0e66581c4492dc7eb206a06
Gyula Lakatos <laxika91@gmail.com>
2015-12-20 18:29:32 +0100

    Started itemdef

---
+++ src/main/java/com/morethanheroic/swords/item/domain/ItemModifier.java
@@ -0,0 +1,19 @@
+package com.morethanheroic.swords.item.domain;
+
+public enum ItemModifier {
+    //Basic
+    MOVEMENT,
+    //Combat
+    MANA,
+    LIFE,
+    INITIATION,
+    ATTACK,
+    MAGIC_ATTACK,
+    MAGIC_DAMAGE,
+    AIMING,
+    DEFENSE,
+    SPELL_RESISTANCE,
+    DAMAGE,
+    RANGED_DAMAGE,
+    //
+}
