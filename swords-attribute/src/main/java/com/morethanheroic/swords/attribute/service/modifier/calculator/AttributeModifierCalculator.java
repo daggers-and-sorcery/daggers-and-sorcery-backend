fce1659941b4a81b62bf5aa711fc5b908e087f94
Gyula Lakatos <laxika91@gmail.com>
2015-12-30 17:14:30 +0100

    Finished attribute refactoring (for now)

--- swords-attribute/src/main/java/com/morethanheroic/swords/attribute/service/modifier/calculator/AttributeModifierCalculator.java
+++ swords-attribute/src/main/java/com/morethanheroic/swords/attribute/service/modifier/calculator/AttributeModifierCalculator.java
@@ -6,6 +6,11 @@ import com.morethanheroic.swords.user.domain.UserEntity;
 
 import java.util.List;
 
+/**
+ * Holds the step by step data of the modifications used while calculated an attribute's value.
+ *
+ * @param <T> The {@link com.morethanheroic.swords.attribute.domain.type.AttributeType} of the attribute this calculator belongs to.
+ */
 public interface AttributeModifierCalculator<T extends Attribute> {
 
     List<AttributeModifierEntry> calculate(UserEntity user, T attribute);
