ad3caea541d7062d08c46d8e71960e2eec0f87f7
laxika <laxika91@gmail.com>
2016-02-18 09:26:21 +0100

    Metadata dao base is done.

---
+++ swords-metadata/src/main/java/com/morethanheroic/swords/metadata/repository/dao/MetadataDatabaseEntity.java
@@ -0,0 +1,13 @@
+package com.morethanheroic.swords.metadata.repository.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MetadataDatabaseEntity {
+
+    private int userId;
+    private int metaId;
+    private int metaValue;
+}
