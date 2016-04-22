9e98fba0b32283065798dc10cf653a685637bf6a
Gyula Lakatos <laxika91@gmail.com>
2016-04-22 19:30:33 +0200

    More refactorings and cleanup

--- swords-combat/src/main/java/com/morethanheroic/swords/combat/service/executor/CombatSettingsAction.java
+++ swords-combat/src/main/java/com/morethanheroic/swords/combat/service/executor/CombatSettingsAction.java
@@ -9,29 +9,22 @@ import com.morethanheroic.swords.item.service.cache.ItemDefinitionCache;
 import com.morethanheroic.swords.settings.service.domain.CombatSettingsEntity;
 import com.morethanheroic.swords.spell.service.UseSpellService;
 import com.morethanheroic.swords.spell.service.cache.SpellDefinitionCache;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
+@RequiredArgsConstructor
 public abstract class CombatSettingsAction {
-
-    private static final Logger logger = LoggerFactory.getLogger(CombatSettingsAction.class);
 
     private final UseItemService useItemService;
     private final ItemDefinitionCache itemDefinitionCache;
     private final UseSpellService useSpellService;
     private final SpellDefinitionCache spellDefinitionCache;
 
-    public CombatSettingsAction(UseItemService useItemService, ItemDefinitionCache itemDefinitionCache, UseSpellService useSpellService, SpellDefinitionCache spellDefinitionCache) {
-        this.useItemService = useItemService;
-        this.itemDefinitionCache = itemDefinitionCache;
-        this.useSpellService = useSpellService;
-        this.spellDefinitionCache = spellDefinitionCache;
-    }
-
     public abstract void executeAction(CombatResult result, Combat combat, CombatSettingsEntity combatSettingsEntity);
 
     protected void executeCombatSettings(UserCombatEntity userEntity, CombatSettingsEntity combatSettingsEntity, CombatEffectDataHolder combatEffectDataHolder) {
-        logger.debug("Running combat settings: " + combatSettingsEntity);
+        log.debug("Running combat settings: " + combatSettingsEntity);
 
         switch (combatSettingsEntity.getType()) {
             case ITEM:
