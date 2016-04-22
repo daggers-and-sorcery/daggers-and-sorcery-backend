3ae770f5c068d93255420e1b582484a1968db2d8
Gyula Lakatos <laxika91@gmail.com>
2016-04-22 19:04:13 +0200

    Added death handling

--- swords-combat/src/main/java/com/morethanheroic/swords/combat/service/calc/result/PlayerDefeatHandler.java
+++ swords-combat/src/main/java/com/morethanheroic/swords/combat/service/calc/result/PlayerDefeatHandler.java
@@ -17,23 +17,29 @@ import java.util.List;
 @RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class PlayerDefeatHandler {
 
+    private static final double PERCENTAGE_TO_REMOVE = 0.10;
+
     private final SkillEntityFactory skillEntityFactory;
     private final HighestSkillCalculator highestSkillCalculator;
 
     public void handleDefeat(Combat combat, CombatResult combatResult) {
-        handleDeath(combat, combatResult);
+        final UserEntity userEntity = combat.getUserCombatEntity().getUserEntity();
+
+        handleDeath(userEntity);
     }
 
-    private void handleDeath(Combat combat, CombatResult combatResult) {
-        final UserEntity userEntity = combat.getUserCombatEntity().getUserEntity();
-        //death mechanism
+    private void handleDeath(UserEntity userEntity) {
         final SkillEntity skillEntity = skillEntityFactory.getSkillEntity(userEntity);
 
-        List<SkillType> highestTreeSkill = highestSkillCalculator.getHighestSkills(skillEntity);
+        final List<SkillType> highestTreeSkill = highestSkillCalculator.getHighestSkills(skillEntity);
+        for (SkillType skillType : highestTreeSkill) {
+            final int experienceToRemove = calculateExperienceToRemove(skillType, skillEntity);
 
-        for (SkillType skill : highestTreeSkill) {
-            //TODO:
-            //skillEntity.removeSkillXp(skill);
+            skillEntity.decreaseExperience(skillType, experienceToRemove);
         }
+    }
+
+    private int calculateExperienceToRemove(SkillType skillType, SkillEntity skillEntity) {
+        return (int) (skillEntity.getExperienceBetweenNextLevel(skillType) * PERCENTAGE_TO_REMOVE);
     }
 }
