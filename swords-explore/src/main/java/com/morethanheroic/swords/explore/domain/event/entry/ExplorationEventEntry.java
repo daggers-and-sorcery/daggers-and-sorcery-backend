29ab64cb1df07fbf88bcb96a93ca349fce862843
Laxika <laxika91@gmail.com>
2016-05-02 23:52:54 +0200

    DS-179: Removed some unused classes.

--- swords-explore/src/main/java/com/morethanheroic/swords/explore/domain/event/entry/ExplorationEventEntry.java
+++
@@ -1,9 +0,0 @@
-package com.morethanheroic.swords.explore.domain.event.entry;
-
-import com.morethanheroic.swords.explore.domain.context.ExplorationContext;
-import com.morethanheroic.swords.explore.domain.event.result.ExplorationEventEntryResult;
-
-public interface ExplorationEventEntry {
-
-    ExplorationEventEntryResult handleExploration(ExplorationContext explorationContext);
-}
