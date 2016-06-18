b3257d1302cf9e721ea3362ec259b7f3bab84a59
Gyula Lakatos <laxika91@gmail.com>
2016-06-18 17:06:42 +0200

    DS-166: More work on smithing, finally smelting is displayed.

--- swords-skill-smithing/src/main/java/com/morethanheroic/swords/skill/smithing/service/recipe/SmithingRecipeConfiguration.java
+++ swords-skill-smithing/src/main/java/com/morethanheroic/swords/skill/smithing/service/recipe/SmithingRecipeConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SmithingRecipeConfiguration {
 
-    @Bean
+    @Bean(name = "smithingSmeltingLearnedRecipeEvaluator")
     public LearnedRecipeEvaluator smithingSmeltingLearnedRecipeEvaluator() {
         return new RecipelessLearnedRecipeEvaluator(SkillType.SMITHING, RecipeType.SMITHING_SMELTING);
     }
--- swords-web/src/main/java/com/morethanheroic/swords/skill/smithing/view/controller/smelting/SmeltingInfoController.java
+++ swords-web/src/main/java/com/morethanheroic/swords/skill/smithing/view/controller/smelting/SmeltingInfoController.java
@@ -1,7 +1,26 @@
 package com.morethanheroic.swords.skill.smithing.view.controller.smelting;
 
+import com.morethanheroic.response.domain.Response;
+import com.morethanheroic.swords.skill.smithing.view.response.domain.configuration.smelting.SmeltingInfoResponseBuilderConfiguration;
+import com.morethanheroic.swords.skill.smithing.view.response.service.smelting.SmeltingInfoResponseBuilder;
+import com.morethanheroic.swords.user.domain.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SmeltingInfoController {
+
+    @Autowired
+    private SmeltingInfoResponseBuilder smeltingInfoResponseBuilder;
+
+    @RequestMapping(value = "/skill/smithing/smelting/info", method = RequestMethod.GET)
+    public Response smeltingInfo(UserEntity userEntity) {
+        return smeltingInfoResponseBuilder.build(
+                SmeltingInfoResponseBuilderConfiguration.builder()
+                        .userEntity(userEntity)
+                        .build()
+        );
+    }
 }
