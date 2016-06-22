aca37f3bf62670590761d2ac9e2915a4f4243561
Laxika <laxika91@gmail.com>
2016-06-22 21:09:09 +0200

    DS-321: Shield give  defense xp from now on.

--- swords-combat/src/main/java/com/morethanheroic/swords/combat/service/awarder/ExperienceAwarder.java
+++ swords-combat/src/main/java/com/morethanheroic/swords/combat/service/awarder/ExperienceAwarder.java
@@ -4,7 +4,7 @@ import com.morethanheroic.swords.combat.domain.CombatResult;
 import com.morethanheroic.swords.combat.service.CombatMessageBuilder;
 import com.morethanheroic.swords.skill.domain.SkillEntity;
 import com.morethanheroic.swords.skill.domain.SkillType;
-import com.morethanheroic.swords.skill.service.SkillFacade;
+import com.morethanheroic.swords.skill.service.factory.SkillEntityFactory;
 import com.morethanheroic.swords.user.domain.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,15 +15,15 @@ import java.util.Map;
 public class ExperienceAwarder {
 
     @Autowired
-    private SkillFacade skillFacade;
+    private SkillEntityFactory skillEntityFactory;
 
     @Autowired
     private CombatMessageBuilder combatMessageBuilder;
 
     public void addXpToUserFromMonsterDefinition(CombatResult result, UserEntity user) {
-        SkillEntity skillEntity = skillFacade.getSkills(user);
+        final SkillEntity skillEntity = skillEntityFactory.getSkillEntity(user);
 
-        Map<SkillType, Integer> rewardXpMap = result.getRewardXpMap();
+        final Map<SkillType, Integer> rewardXpMap = result.getRewardXpMap();
 
         for (Map.Entry<SkillType, Integer> rewardEntity : rewardXpMap.entrySet()) {
             result.addMessage(combatMessageBuilder.buildXpRewardMessage(rewardEntity.getKey().name(), rewardEntity.getValue()));
