bbd36f376edd397f78fbb9ce99a9e374c58bb47e
laxika <laxika91@gmail.com>
2016-06-29 10:09:24 +0200

    Quickfixes

--- swords-event/src/main/java/com/morethanheroic/swords/event/service/EventProcessor.java
+++ swords-event/src/main/java/com/morethanheroic/swords/event/service/EventProcessor.java
@@ -1,19 +1,15 @@
 package com.morethanheroic.swords.event.service;
 
-import com.morethanheroic.swords.event.repository.dao.EventDatabaseEntity;
-import com.morethanheroic.swords.event.repository.domain.EventMapper;
-import com.morethanheroic.swords.event.service.event.Event;
-import com.morethanheroic.swords.user.service.UserFacade;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.morethanheroic.swords.event.repository.dao.EventDatabaseEntity;
+import com.morethanheroic.swords.event.repository.domain.EventMapper;
+import com.morethanheroic.swords.user.service.UserFacade;
 
 @Service
 public class EventProcessor {
@@ -28,7 +24,7 @@ public class EventProcessor {
     private UserFacade userFacade;
 
     @Transactional
-    //@Scheduled(fixedRate = 100)
+    @Scheduled(fixedRate = 100)
     public void processEvents() {
         final List<EventDatabaseEntity> endingEvents = eventMapper.getEndingEvents();
 
