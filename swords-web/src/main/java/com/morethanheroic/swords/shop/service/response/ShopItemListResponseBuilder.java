ff98871a3dc23f1ef00e20f3b927ad9cd08ef20b
Gyula Lakatos <laxika91@gmail.com>
2016-07-10 18:36:11 +0200

    Moved shop to it's own module.

--- swords-web/src/main/java/com/morethanheroic/swords/shop/service/response/ShopItemListResponseBuilder.java
+++ swords-web/src/main/java/com/morethanheroic/swords/shop/service/response/ShopItemListResponseBuilder.java
@@ -21,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
