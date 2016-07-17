4687703af0fa9fe01f13d89222f09c2c24cf1253
Gyula Lakatos <laxika91@gmail.com>
2016-07-17 17:50:55 +0200

    DS-261: multi location fixes

--- swords-web/src/main/java/com/morethanheroic/swords/explore/view/editor/ExplorationEventLocationTypeEditor.java
+++ swords-web/src/main/java/com/morethanheroic/swords/explore/view/editor/ExplorationEventLocationTypeEditor.java
@@ -8,6 +8,12 @@ public class ExplorationEventLocationTypeEditor extends PropertyEditorSupport {
 
     @Override
     public void setAsText(final String text) {
+        if (text.equals("null")) {
+            setValue(null);
+            
+            return;
+        }
+
         setValue(ExplorationEventLocationType.valueOf(text.toUpperCase()));
     }
 }
