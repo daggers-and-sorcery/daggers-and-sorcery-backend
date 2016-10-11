c5c73438e6c4206fc7fa8a7394c5be7a52dc7b6c
Gyula Lakatos <laxika91@gmail.com>
2016-10-11 22:19:22 +0200

    More terrain support

--- swords-explore/src/main/java/com/morethanheroic/swords/explore/domain/event/ExplorationEventTerrain.java
+++ swords-explore/src/main/java/com/morethanheroic/swords/explore/domain/event/ExplorationEventTerrain.java
@@ -1,5 +1,5 @@
 package com.morethanheroic.swords.explore.domain.event;
 
 public enum ExplorationEventTerrain {
-    FIELD, FOREST, DESERT, CITY
+    FIELD, FOREST, DESERT, CITY, FARM, INN, FORESTER
 }
