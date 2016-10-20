ac10867a6137b5733f75512dee75bf9008598f38
laxika <laxika91@gmail.com>
2016-10-20 20:32:33 +0200

    The inn restore mana too. Removed the admin page.

--- swords-inn/src/main/java/com/morethanheroic/swords/inn/service/server/InnServiceServer.java
+++ swords-inn/src/main/java/com/morethanheroic/swords/inn/service/server/InnServiceServer.java
@@ -3,11 +3,10 @@ package com.morethanheroic.swords.inn.service.server;
 import com.morethanheroic.swords.inn.domain.service.ServiceType;
 import com.morethanheroic.swords.inn.service.server.context.ServingContext;
 import com.morethanheroic.swords.inn.service.server.executor.ServingExecutor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
+import java.util.Collections;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
@@ -15,23 +14,21 @@ import java.util.Map;
 @Service
 public class InnServiceServer {
 
-    @Autowired
-    private List<ServingExecutor> servingExecutors;
+    private final Map<ServiceType, ServingExecutor<ServingContext>> executorMap;
 
-    private Map<ServiceType, ServingExecutor<ServingContext>> executorMap;
+    public InnServiceServer(List<ServingExecutor> servingExecutors) {
+        final Map<ServiceType, ServingExecutor<ServingContext>> executorMap = new EnumMap<>(ServiceType.class);
 
-    @PostConstruct
-    private void initialize() {
-        executorMap = new EnumMap<>(ServiceType.class);
-
-        for (ServingExecutor servingExecutor : servingExecutors) {
+        for (ServingExecutor<ServingContext> servingExecutor : servingExecutors) {
             executorMap.put(servingExecutor.supportedService(), servingExecutor);
         }
+
+        this.executorMap = Collections.unmodifiableMap(executorMap);
     }
 
     @Transactional
     public boolean serve(ServiceType serviceType, ServingContext servingContext) {
-        final ServingExecutor servingExecutor = executorMap.get(serviceType);
+        final ServingExecutor<ServingContext> servingExecutor = executorMap.get(serviceType);
 
         if (servingExecutor.canExecute(servingContext)) {
             servingExecutor.executeServing(servingContext);
