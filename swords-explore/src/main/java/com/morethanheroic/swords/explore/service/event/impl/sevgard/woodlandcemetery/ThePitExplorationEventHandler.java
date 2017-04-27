3ff1237726e9ec0c3cc4b4ccc2ef63c6511e520b
laxika <laxika91@gmail.com>
2017-04-27 22:33:06 +0200

    Working on the pit event.

---
+++ swords-explore/src/main/java/com/morethanheroic/swords/explore/service/event/impl/sevgard/woodlandcemetery/ThePitExplorationEventHandler.java
@@ -0,0 +1,134 @@
+package com.morethanheroic.swords.explore.service.event.impl.sevgard.woodlandcemetery;
+
+import com.morethanheroic.swords.attribute.service.manipulator.UserBasicAttributeManipulator;
+import com.morethanheroic.swords.explore.domain.ExplorationResult;
+import com.morethanheroic.swords.explore.service.event.ExplorationEvent;
+import com.morethanheroic.swords.explore.service.event.newevent.ExplorationContext;
+import com.morethanheroic.swords.explore.service.event.newevent.ExplorationResultStageBuilderFactory;
+import com.morethanheroic.swords.explore.service.event.newevent.ImprovedExplorationEventHandler;
+import com.morethanheroic.swords.explore.service.event.newevent.ReplyOption;
+import com.morethanheroic.swords.inventory.service.InventoryEntityFactory;
+import com.morethanheroic.swords.item.service.cache.ItemDefinitionCache;
+import com.morethanheroic.swords.user.domain.UserEntity;
+import lombok.RequiredArgsConstructor;
+
+import static com.morethanheroic.swords.attribute.domain.GeneralAttribute.DEXTERITY;
+
+@ExplorationEvent
+@RequiredArgsConstructor
+public class ThePitExplorationEventHandler extends ImprovedExplorationEventHandler {
+
+    private static final int EVENT_ID = 23;
+
+    private static final int SKELETON_MONSTER_ID = 1;
+
+    private static final int HEALT_LOSS_AMOUNT = 5;
+
+    private static final int STARTER_STAGE = 0;
+    private static final int FIRST_COMBAT_STAGE = 1;
+    private static final int SECOND_COMBAT_STAGE = 2;
+    private static final int GO_DEEPER_STAGE = 3;
+    private static final int GO_TO_EXIT_STAGE = 4;
+
+    private static final int TORCH_ID = 120;
+    private static final int BRONZE_COIN_ID = 1;
+
+    private final ExplorationResultStageBuilderFactory explorationResultStageBuilderFactory;
+    private final UserBasicAttributeManipulator userBasicAttributeManipulator;
+    private final InventoryEntityFactory inventoryEntityFactory;
+    private final ItemDefinitionCache itemDefinitionCache;
+
+    @Override
+    public ExplorationResult handleExplore(ExplorationContext explorationContext) {
+        final UserEntity userEntity = explorationContext.getUserEntity();
+
+        return explorationResultStageBuilderFactory.newBuilder()
+                .addStage(STARTER_STAGE,
+                        explorationResultBuilder1 -> explorationResultBuilder1
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_1")
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_2")
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_3")
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_4")
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_5")
+                                .newCombatEntry(SKELETON_MONSTER_ID, EVENT_ID, FIRST_COMBAT_STAGE)
+                                .build()
+                )
+                .addStage(FIRST_COMBAT_STAGE,
+                        explorationResultBuilder1 -> explorationResultBuilder1
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_6")
+                                .newAttributeAttemptEntry(DEXTERITY, 8)
+                                .isSuccess((explorationResultBuilder) -> explorationResultBuilder
+                                        .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_8")
+                                        .newCombatEntry(SKELETON_MONSTER_ID, EVENT_ID, SECOND_COMBAT_STAGE)
+                                        .build()
+                                )
+                                .isFailure((explorationResultBuilder) -> explorationResultBuilder
+                                        .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_7")
+                                        .newCustomLogicEntry(() -> userBasicAttributeManipulator.increaseHealth(userEntity, HEALT_LOSS_AMOUNT))
+                                        .newCombatEntry(SKELETON_MONSTER_ID, EVENT_ID, SECOND_COMBAT_STAGE)
+                                        .build()
+                                )
+                                .build()
+                )
+                .addStage(SECOND_COMBAT_STAGE,
+                        explorationResultBuilder1 -> explorationResultBuilder1
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_9")
+                                .newOptionEntry(
+                                        ReplyOption.builder()
+                                                .message("THE_PIT_EXPLORATION_EVENT_QUESTION_REPLY_1")
+                                                .stage(GO_DEEPER_STAGE)
+                                                .build(),
+                                        ReplyOption.builder()
+                                                .message("THE_PIT_EXPLORATION_EVENT_QUESTION_REPLY_2")
+                                                .stage(GO_TO_EXIT_STAGE)
+                                                .build()
+                                )
+                                .build()
+                )
+                .addStage(GO_TO_EXIT_STAGE,
+                        explorationResultBuilder1 -> explorationResultBuilder1
+                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_10")
+                                .resetExploration()
+                                .build()
+                )
+                .addStage(GO_DEEPER_STAGE,
+                        explorationResultBuilder1 -> explorationResultBuilder1
+                                .newHasItemMultiWayPath(explorationContext, TORCH_ID)
+                                .isSuccess(
+                                        explorationResultBuilder2 -> explorationResultBuilder2
+                                                //TODO: Successfully have the torch
+                                                //Yes: You take out your torch and light it before going into the corridor. ...
+                                                .build()
+                                )
+                                .isFailure(
+                                        explorationResultBuilder2 -> explorationResultBuilder2
+                                                .newMessageEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_11")
+                                                .newCustomLogicEntry(() ->
+                                                        inventoryEntityFactory.getEntity(userEntity).addItem(itemDefinitionCache.getDefinition(BRONZE_COIN_ID), 30)
+                                                )
+                                                .newMessageBoxEntry("THE_PIT_EXPLORATION_EVENT_ENTRY_17")
+                                                .resetExploration()
+                                                .build()
+                                )
+                                .build()
+                )
+                .runStage(explorationContext);
+    }
+
+    @Override
+    public ExplorationResult handleInfo(ExplorationContext explorationContext) {
+        //TODO!!!!!
+        return null;
+    }
+
+    @Override
+    public boolean isValidNextStageAtStage(int stage, int nextStage) {
+        //TODO
+        return true;
+    }
+
+    @Override
+    public int getId() {
+        return EVENT_ID;
+    }
+}
