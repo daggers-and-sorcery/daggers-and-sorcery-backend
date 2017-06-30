88e212cd8d5d21ed6dd4e8d68f65da4fa8190d73
laxika <laxika91@gmail.com>
2017-06-30 18:26:10 +0200

    Small fix

--- swords-web/src/main/java/com/morethanheroic/swords/explore/view/response/domain/RefreshUserDataEventPartialResponse.java
+++ swords-web/src/main/java/com/morethanheroic/swords/explore/view/response/domain/RefreshUserDataEventPartialResponse.java
@@ -2,7 +2,9 @@ package com.morethanheroic.swords.explore.view.response.domain;
 
 import com.morethanheroic.response.domain.PartialResponse;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class RefreshUserDataEventPartialResponse extends PartialResponse {
 
