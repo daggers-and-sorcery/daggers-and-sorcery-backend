55a9bbc789229c6c7bce06055cdacdcb4655941a
Gyula Lakatos <v-glakatos@hotels.com>
2017-07-04 11:21:12 +0200

    Working on special attributes

--- swords-combat-special-attribute/src/main/java/com/morethanheroic/swords/combat/calculator/damage/ExtraDamageAgainstMonsterTypeDamageCombatEventHandler.java
+++ swords-combat-special-attribute/src/main/java/com/morethanheroic/swords/combat/calculator/damage/ExtraDamageAgainstMonsterTypeDamageCombatEventHandler.java
@@ -3,7 +3,8 @@ package com.morethanheroic.swords.combat.calculator.damage;
 import com.google.common.collect.ImmutableMap;
 import com.morethanheroic.swords.attribute.domain.SpecialAttribute;
 import com.morethanheroic.swords.attribute.service.calc.GlobalAttributeCalculator;
-import com.morethanheroic.swords.attribute.service.calc.domain.calculation.SimpleValueAttributeCalculationResult;
+import com.morethanheroic.swords.attribute.service.calc.domain.calculation.DiceValueAttributeCalculationResult;
+import com.morethanheroic.swords.combat.bonus.CombatBonusTransformer;
 import com.morethanheroic.swords.combat.bonus.domain.CombatBonus;
 import com.morethanheroic.swords.combat.entity.CombatEntityUtil;
 import com.morethanheroic.swords.combat.entity.domain.CombatEntity;
@@ -31,40 +32,36 @@ public class ExtraDamageAgainstMonsterTypeDamageCombatEventHandler implements Da
 
     private final CombatEntityUtil combatEntityUtil;
     private final GlobalAttributeCalculator globalAttributeCalculator;
+    private final CombatBonusTransformer combatBonusTransformer;
 
     @Override
     public DamageEventCalculationResult handleEvent(final CombatEntity damagingEntity, final CombatEntity damagedEntity, final DamageEventCalculationContext damageEventCalculationContext) {
         if (combatEntityUtil.isPlayer(damagingEntity)) {
             //TODO: Add combat messages!!
-            int extraDamage = 0;
+            CombatBonus combatBonus = CombatBonus.EMPTY_COMBAT_BONUS;
 
             final UserEntity userEntity = ((UserCombatEntity) damagingEntity).getUserEntity();
             final MonsterDefinition monsterDefinition = ((MonsterCombatEntity) damagedEntity).getMonsterDefinition();
 
-            extraDamage += calculateExtraDamageForType(userEntity, monsterDefinition.getType());
-            extraDamage += calculateExtraDamageForType(userEntity, monsterDefinition.getSubtype());
+            combatBonus = combatBonus.add(calculateExtraDamageForType(userEntity, monsterDefinition.getType()));
+            combatBonus = combatBonus.add(calculateExtraDamageForType(userEntity, monsterDefinition.getSubtype()));
 
             return DamageEventCalculationResult.builder()
                     .combatSteps(Collections.emptyList())
-                    .bonusDamage(
-                            CombatBonus.builder()
-                                    .value(extraDamage)
-                                    .build()
-                    )
+                    .bonusDamage(combatBonus)
                     .build();
         }
 
         return DamageEventCalculationResult.EMPTY_RESULT;
     }
 
-    private int calculateExtraDamageForType(final UserEntity userEntity, final MonsterType monsterType) {
+    private CombatBonus calculateExtraDamageForType(final UserEntity userEntity, final MonsterType monsterType) {
         if (MONSTER_TYPE_SPECIAL_ATTRIBUTE_MAP.containsKey(monsterType)) {
-            //TODO: Be able to add d2/d4 etc damages here too
-            final SimpleValueAttributeCalculationResult attributeCalculationResult = globalAttributeCalculator.calculateActualValue(userEntity, MONSTER_TYPE_SPECIAL_ATTRIBUTE_MAP.get(monsterType));
+            final DiceValueAttributeCalculationResult attributeCalculationResult = (DiceValueAttributeCalculationResult) globalAttributeCalculator.calculateActualValue(userEntity, MONSTER_TYPE_SPECIAL_ATTRIBUTE_MAP.get(monsterType));
 
-            return attributeCalculationResult.getValue();
+            return combatBonusTransformer.createFrom(attributeCalculationResult);
         }
 
-        return 0;
+        return CombatBonus.EMPTY_COMBAT_BONUS;
     }
 }
