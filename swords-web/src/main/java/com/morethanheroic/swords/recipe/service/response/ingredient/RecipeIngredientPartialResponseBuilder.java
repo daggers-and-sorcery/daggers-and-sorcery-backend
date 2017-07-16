06f4c86f999ada5fd37b69eac3be8be25161ae10
Gyula Lakatos <laxika91@gmail.com>
2017-07-16 11:35:22 +0200

    Added new certs

--- swords-web/src/main/java/com/morethanheroic/swords/recipe/service/response/ingredient/RecipeIngredientPartialResponseBuilder.java
+++ swords-web/src/main/java/com/morethanheroic/swords/recipe/service/response/ingredient/RecipeIngredientPartialResponseBuilder.java
@@ -1,16 +1,15 @@
 package com.morethanheroic.swords.recipe.service.response.ingredient;
 
+import com.morethanheroic.response.service.PartialResponseBuilder;
 import com.morethanheroic.swords.item.service.cache.ItemDefinitionCache;
 import com.morethanheroic.swords.recipe.domain.RecipeIngredient;
-import com.morethanheroic.response.service.PartialResponseBuilder;
 import com.morethanheroic.swords.recipe.service.response.ingredient.domain.RecipeIngredientPartialResponse;
 import com.morethanheroic.swords.recipe.service.response.ingredient.domain.RecipeIngredientPartialResponseBuilderConfiguration;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor
 public class RecipeIngredientPartialResponseBuilder implements PartialResponseBuilder<RecipeIngredientPartialResponseBuilderConfiguration> {
 
     private final ItemDefinitionCache itemDefinitionCache;
