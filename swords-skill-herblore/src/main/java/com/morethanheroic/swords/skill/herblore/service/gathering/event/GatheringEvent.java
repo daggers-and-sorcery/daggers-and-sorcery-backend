20f4b3177c208e3d326fa86ad2575eeb7e96ad36
laxika <laxika91@gmail.com>
2017-07-26 22:19:37 +0200

    Fixing a few things

--- swords-skill-herblore/src/main/java/com/morethanheroic/swords/skill/herblore/service/gathering/event/GatheringEvent.java
+++ swords-skill-herblore/src/main/java/com/morethanheroic/swords/skill/herblore/service/gathering/event/GatheringEvent.java
@@ -3,9 +3,14 @@ package com.morethanheroic.swords.skill.herblore.service.gathering.event;
 import com.morethanheroic.swords.event.domain.Event;
 import com.morethanheroic.swords.inventory.service.InventoryEntityFactory;
 import com.morethanheroic.swords.item.service.cache.ItemDefinitionCache;
+import com.morethanheroic.swords.skill.domain.SkillEntity;
+import com.morethanheroic.swords.skill.domain.SkillType;
+import com.morethanheroic.swords.skill.service.factory.SkillEntityFactory;
 import com.morethanheroic.swords.user.domain.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 /**
  * Controls what should happen when a gathering event finishes.
@@ -16,9 +21,12 @@ public class GatheringEvent implements Event {
 
     private static final int GATHERING_EVENT_ID = 4;
     private static final int KINGS_BLOOD_ID = 185;
+    private static final int SILVERLEAF_ID = 202;
 
     private final InventoryEntityFactory inventoryEntityFactory;
     private final ItemDefinitionCache itemDefinitionCache;
+    private final SkillEntityFactory skillEntityFactory;
+    private final Random random;
 
     @Override
     public int getId() {
@@ -27,7 +35,20 @@ public class GatheringEvent implements Event {
 
     @Override
     public void processEvent(final UserEntity userEntity) {
-        //TODO: Add variation based on herblore level
+        final SkillEntity skillEntity = skillEntityFactory.getEntity(userEntity);
+        boolean herbAwarded = false;
+
         inventoryEntityFactory.getEntity(userEntity).addItem(itemDefinitionCache.getDefinition(KINGS_BLOOD_ID), 1);
+
+        if (skillEntity.getLevel(SkillType.HERBLORE) > 5) {
+            if (random.nextInt(100) <= 5) {
+                herbAwarded = true;
+                inventoryEntityFactory.getEntity(userEntity).addItem(itemDefinitionCache.getDefinition(SILVERLEAF_ID), 1);
+            }
+        }
+
+        if (!herbAwarded) {
+            inventoryEntityFactory.getEntity(userEntity).addItem(itemDefinitionCache.getDefinition(KINGS_BLOOD_ID), 1);
+        }
     }
 }
