59335a257aa4f047da94e446b80ee70cdcd580f8
laxika <laxika91@gmail.com>
2017-08-13 23:14:13 +0200

    Fixed the spell items in messages

--- swords-combat/src/main/java/com/morethanheroic/swords/spell/service/UseSpellService.java
+++ swords-combat/src/main/java/com/morethanheroic/swords/spell/service/UseSpellService.java
@@ -2,7 +2,6 @@ package com.morethanheroic.swords.spell.service;
 
 import com.google.common.collect.Lists;
 import com.morethanheroic.swords.attribute.service.calc.GlobalAttributeCalculator;
-import com.morethanheroic.swords.attribute.service.manipulator.UserBasicAttributeManipulator;
 import com.morethanheroic.swords.combat.domain.CombatContext;
 import com.morethanheroic.swords.combat.domain.CombatEffectDataHolder;
 import com.morethanheroic.swords.combat.domain.effect.CombatEffectApplyingContext;
@@ -13,8 +12,11 @@ import com.morethanheroic.swords.combat.step.domain.CombatStep;
 import com.morethanheroic.swords.combat.step.domain.DefaultCombatStep;
 import com.morethanheroic.swords.combat.step.message.CombatMessageFactory;
 import com.morethanheroic.swords.effect.domain.EffectSettingDefinitionHolder;
+import com.morethanheroic.swords.inventory.domain.IdentificationType;
 import com.morethanheroic.swords.inventory.domain.InventoryEntity;
 import com.morethanheroic.swords.inventory.service.InventoryEntityFactory;
+import com.morethanheroic.swords.item.domain.ItemDefinition;
+import com.morethanheroic.swords.item.service.definition.cache.ItemDefinitionCache;
 import com.morethanheroic.swords.spell.domain.CostType;
 import com.morethanheroic.swords.spell.domain.SpellCost;
 import com.morethanheroic.swords.spell.domain.SpellDefinition;
@@ -36,8 +38,8 @@ public class UseSpellService {
     private final UserMapper userMapper;
     private final GlobalAttributeCalculator globalAttributeCalculator;
     private final CombatMessageFactory combatMessageFactory;
-    private final UserBasicAttributeManipulator userBasicAttributeManipulator;
     private final InventoryEntityFactory inventoryEntityFactory;
+    private final ItemDefinitionCache itemDefinitionCache;
 
     public boolean canUseSpell(UserEntity userEntity, SpellDefinition spell) {
         InventoryEntity inventoryEntity = inventoryEntityFactory.getEntity(userEntity);
@@ -57,16 +59,30 @@ public class UseSpellService {
         return true;
     }
 
-    public boolean canUseSpell(UserCombatEntity combatEntity, SpellDefinition spell) {
+    public boolean canUseSpell(UserCombatEntity combatEntity, SpellDefinition spell, List<CombatStep> combatSteps) {
         InventoryEntity inventoryEntity = inventoryEntityFactory.getEntity(combatEntity.getUserEntity());
 
         for (SpellCost rawSpellCost : spell.getSpellCosts()) {
             if (rawSpellCost.getType() == CostType.ITEM) {
-                if (!inventoryEntity.hasItemAmount(rawSpellCost.getId(), rawSpellCost.getAmount(), true)) {
+                final ItemDefinition itemDefinition = itemDefinitionCache.getDefinition(rawSpellCost.getId());
+
+                if (!inventoryEntity.hasItemAmount(itemDefinition, rawSpellCost.getAmount(), IdentificationType.IDENTIFIED)) {
+                    combatSteps.add(
+                            DefaultCombatStep.builder()
+                                    .message(combatMessageFactory.newMessage("spell", "COMBAT_MESSAGE_SPELL_CAST_NOT_ENOUGH_ITEM", itemDefinition.getName()))
+                                    .build()
+                    );
+
                     return false;
                 }
             } else if (rawSpellCost.getType() == CostType.MANA) {
                 if (combatEntity.getActualMana() < rawSpellCost.getAmount()) {
+                    combatSteps.add(
+                            DefaultCombatStep.builder()
+                                    .message(combatMessageFactory.newMessage("spell", "COMBAT_MESSAGE_SPELL_CAST_NOT_ENOUGH_MANA"))
+                                    .build()
+                    );
+
                     return false;
                 }
             }
@@ -85,7 +101,7 @@ public class UseSpellService {
     public List<CombatStep> useSpell(CombatContext combatContext, SpellDefinition spell, CombatEffectDataHolder combatEffectDataHolder) {
         final List<CombatStep> combatSteps = new ArrayList<>();
 
-        if (canUseSpell(combatContext.getUser(), spell)) {
+        if (canUseSpell(combatContext.getUser(), spell, combatSteps)) {
             combatSteps.add(
                     DefaultCombatStep.builder()
                             .message(combatMessageFactory.newMessage("spell", "COMBAT_MESSAGE_SPELL_CAST", spell.getName()))
@@ -93,12 +109,6 @@ public class UseSpellService {
             );
 
             combatSteps.addAll(applySpell(combatContext, spell, combatEffectDataHolder));
-        } else {
-            combatSteps.add(
-                    DefaultCombatStep.builder()
-                            .message(combatMessageFactory.newMessage("spell", "COMBAT_MESSAGE_SPELL_CAST_NOT_ENOUGH_MANA"))
-                            .build()
-            );
         }
 
         final UserCombatEntity combatEntity = combatContext.getUser();
