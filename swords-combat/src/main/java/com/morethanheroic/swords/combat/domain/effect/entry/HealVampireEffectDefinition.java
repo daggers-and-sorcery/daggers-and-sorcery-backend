74a976e7da5b9bbd4ed1a2ccf68f0e5bc74e70e7
laxika <laxika91@gmail.com>
2017-09-11 12:52:04 +0200

    Fixed the blood vial

--- swords-combat/src/main/java/com/morethanheroic/swords/combat/domain/effect/entry/HealVampireEffectDefinition.java
+++ swords-combat/src/main/java/com/morethanheroic/swords/combat/domain/effect/entry/HealVampireEffectDefinition.java
@@ -3,11 +3,9 @@ package com.morethanheroic.swords.combat.domain.effect.entry;
 import com.morethanheroic.swords.attribute.domain.CombatAttribute;
 import com.morethanheroic.swords.attribute.service.calc.GlobalAttributeCalculator;
 import com.morethanheroic.swords.attribute.service.calc.domain.data.AttributeData;
-import com.morethanheroic.swords.attribute.service.manipulator.UserBasicAttributeManipulator;
 import com.morethanheroic.swords.combat.domain.effect.CombatEffectApplyingContext;
 import com.morethanheroic.swords.combat.domain.effect.ImprovedCombatEffectDefinition;
 import com.morethanheroic.swords.combat.entity.domain.UserCombatEntity;
-import com.morethanheroic.swords.user.domain.UserEntity;
 import com.morethanheroic.swords.vampire.service.VampireCalculator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,7 +22,6 @@ public class HealVampireEffectDefinition extends ImprovedCombatEffectDefinition 
     private static final int MINIMUM_HEALTH = 1;
 
     private final VampireCalculator vampireCalculator;
-    private final UserBasicAttributeManipulator basicAttributeManipulator;
     private final GlobalAttributeCalculator globalAttributeCalculator;
 
     @Override
@@ -32,23 +29,23 @@ public class HealVampireEffectDefinition extends ImprovedCombatEffectDefinition 
         final int healAmount = effectApplyingContext.getEffectSettings().getSettingAsInt("heal-amount");
         final int damageAmount = effectApplyingContext.getEffectSettings().getSettingAsInt("damage-amount");
 
-        final UserEntity userEntity = ((UserCombatEntity) effectApplyingContext.getDestination().getCombatEntity()).getUserEntity();
+        final UserCombatEntity userCombatEntity = (UserCombatEntity) effectApplyingContext.getDestination().getCombatEntity();
 
-        if (vampireCalculator.isVampire(userEntity)) {
-            basicAttributeManipulator.increaseHealth(userEntity, healAmount);
+        if (vampireCalculator.isVampire(userCombatEntity.getUserEntity())) {
+            userCombatEntity.increaseActualHealth(healAmount);
         } else {
-            final AttributeData attributeData = globalAttributeCalculator.calculateAttributeValue(userEntity, CombatAttribute.LIFE);
+            final AttributeData attributeData = globalAttributeCalculator.calculateAttributeValue(userCombatEntity.getUserEntity(), CombatAttribute.LIFE);
 
             if (attributeData.getActual().getValue() - damageAmount <= MINIMUM_HEALTH) {
-                basicAttributeManipulator.decreaseHealth(userEntity, MINIMUM_HEALTH);
+                userCombatEntity.decreaseActualHealth(MINIMUM_HEALTH);
             } else {
-                basicAttributeManipulator.decreaseHealth(userEntity, damageAmount);
+                userCombatEntity.decreaseActualHealth(damageAmount);
             }
         }
     }
 
     @Override
     public String getId() {
-        return "heal_vampire";
+        return "heal_vampire_damage_human";
     }
 }
