d06c6fbddfaf7a8f6ae5c0334d3c5d8d9ffa7183
Gyula Lakatos <laxika91@gmail.com>
2017-10-08 21:23:51 +0200

    Working on the offloading of modules

--- swords-race-definition/src/main/java/com/morethanheroic/swords/race/model/Race.java
+++
@@ -1,8 +0,0 @@
-package com.morethanheroic.swords.race.model;
-
-/**
- * Holds the static data of the races in the game.
- */
-public enum Race {
-    HUMAN, ORC, DWARF, ELF, DARK_ELF, LIZARDMEN, GNOME, DRACONIC
-}
--- swords-race-definition/src/main/java/com/morethanheroic/swords/race/service/loader/entity/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains raw data of the race definitions used when loading the xml files.
- */
-@XmlSchema(namespace = "http://www.swordsandsorcery.com/race", elementFormDefault = XmlNsForm.QUALIFIED)
-package com.morethanheroic.swords.race.service.loader.entity;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
