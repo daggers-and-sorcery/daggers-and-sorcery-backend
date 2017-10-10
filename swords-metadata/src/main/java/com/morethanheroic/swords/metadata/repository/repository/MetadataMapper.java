456dc516af2af7cf6934d118f257eceda9247d8f
laxika <laxika91@gmail.com>
2017-10-11 00:04:15 +0200

    Working on the new exploration place

--- swords-metadata/src/main/java/com/morethanheroic/swords/metadata/repository/repository/MetadataMapper.java
+++ swords-metadata/src/main/java/com/morethanheroic/swords/metadata/repository/repository/MetadataMapper.java
@@ -1,6 +1,7 @@
 package com.morethanheroic.swords.metadata.repository.repository;
 
 import com.morethanheroic.swords.metadata.repository.dao.MetadataDatabaseEntity;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -14,4 +15,7 @@ public interface MetadataMapper {
 
     @Insert("INSERT INTO user_meta SET user_id = #{user_id}, meta_id = #{meta_id}, meta_value = #{meta_value} ON DUPLICATE KEY UPDATE meta_value = #{meta_value}")
     void setMetadata(@Param("user_id") int userId, @Param("meta_id") int metaId, @Param("meta_value") int metaValue);
+
+    @Delete("DELETE FROM user_meta WHERE meta_id = #{meta_id}")
+    void deleteAllMetadataWithId(@Param("meta_id") int metaId);
 }
--- swords-zone/src/main/java/com/morethanheroic/swords/zone/service/definition/transformer/ZoneDefinitionTransformer.java
+++ swords-zone/src/main/java/com/morethanheroic/swords/zone/service/definition/transformer/ZoneDefinitionTransformer.java
@@ -3,6 +3,7 @@ package com.morethanheroic.swords.zone.service.definition.transformer;
 import com.morethanheroic.definition.transformer.DefinitionTransformer;
 import com.morethanheroic.swords.zone.domain.ExplorationZone;
 import com.morethanheroic.swords.zone.domain.ZoneDefinition;
+import com.morethanheroic.swords.zone.service.definition.cache.accessibility.ZoneAccessibilityDefinition;
 import com.morethanheroic.swords.zone.service.definition.loader.domain.RawZoneDefinition;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class ZoneDefinitionTransformer implements DefinitionTransformer<ZoneDefinition, RawZoneDefinition> {
 
     @Override
-    public ZoneDefinition transform(RawZoneDefinition rawDefinition) {
+    public ZoneDefinition transform(final RawZoneDefinition rawDefinition) {
         return ZoneDefinition.builder()
                 .id(ExplorationZone.valueOf(rawDefinition.getId()))
                 .name(rawDefinition.getName())
@@ -19,6 +20,15 @@ public class ZoneDefinitionTransformer implements DefinitionTransformer<ZoneDefi
                 .enabled(rawDefinition.isEnabled())
                 .minimumLevel(rawDefinition.getMinimumLevel())
                 .maximumLevel(rawDefinition.getMaximumLevel())
+                .accessibility(transformAccessibility(rawDefinition))
                 .build();
+    }
+
+    private ZoneAccessibilityDefinition transformAccessibility(final RawZoneDefinition rawDefinition) {
+        return rawDefinition.getAccessibility() == null ? null :
+                ZoneAccessibilityDefinition.builder()
+                        .rechargeRate(rawDefinition.getAccessibility().getRecharge())
+                        .count(rawDefinition.getAccessibility().getCount())
+                        .build();
     }
 }
